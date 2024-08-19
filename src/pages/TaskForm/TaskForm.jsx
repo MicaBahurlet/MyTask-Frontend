f3c85e0423ec9a4d8ae7eebf6545a1d6b3cbfa42
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-08-19 16:41:36 -0300

    redirect to dashboard later update and create task

--- src/pages/TaskForm/TaskForm.jsx
+++ src/pages/TaskForm/TaskForm.jsx
@@ -34,7 +34,7 @@ function TaskForm() {
       }
     };
     loadTask();
-  }, []);
+  }, [params.id, getTask]);
 
   return (
     <FormContainer>
@@ -46,9 +46,10 @@ function TaskForm() {
         onSubmit={async (values, actions) => {
           if (params.id) {
             await updateTask(params.id, values);
-            navigate("/");
+            navigate("/dashboard"); // Redirige a /dashboard después de actualizar
           } else {
             await createTask(values);
+            navigate("/dashboard"); // Redirige a /dashboard después de crear
           }
           setTask({
             title: "",
@@ -87,91 +88,3 @@ function TaskForm() {
 }
 
 export default TaskForm;
-
-
-
-// import { Formik, Form } from "formik";
-// import { useTasks } from "../../context/taskContext.jsx";
-// import { useParams } from "react-router-dom";
-// import { useEffect, useState } from "react";
-// import { useNavigate } from "react-router-dom";
-
-// function TaskForm() {
-//   const { createTask, getTask, updateTask } = useTasks();
-//   const [task, setTask] = useState({
-//     title: "",
-//     description: "",
-//   });
-//   const params = useParams();
-
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     const loadTask = async () => {
-//       if (params.id) {
-//        const task = await getTask(params.id);
-//        console.log(task);
-//        setTask({
-//          title: task.title,
-//          description: task.description,
-//        });
-//       }
-//     };
-//     loadTask();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>{params.id ? "Editar Tarea" : "Crear Tarea"}</h1>
-
-//       <Formik
-//         initialValues={task} //para usar mi estado de tarea
-//         enableReinitialize={true}
-//         onSubmit={async (values, actions) => {
-//           console.log(values);
-          
-//           if (params.id) {
-//            await updateTask(params.id, values);
-//             navigate("/");
-//           } else {
-//             await createTask(values);
-//           }
-//           setTask({
-//             title: "",
-//             description: "",
-//           });
-//           // actions.resetForm();
-//         }}
-//       >
-//         {({ handleChange, handleSubmit, values, isSubmitting }) => (
-//           <Form onSubmit={handleSubmit}>
-//             <label>Title</label>
-//             <input
-//               type="text"
-//               name="title"
-//               placeholder="Tarea"
-//               onChange={handleChange}
-//               value={values.title}
-//             />
-
-//             <label>Description</label>
-//             <textarea
-//               type="text"
-//               name="description"
-//               rows="3"
-//               placeholder="Descripción de tarea"
-//               onChange={handleChange}
-//               value={values.description}
-//             />
-
-//             <button type="submit" disabled={isSubmitting}>
-//               {isSubmitting ? "Guardando..." : "Guardar"}
-//             </button>
-//           </Form>
-//         )}
-//       </Formik>
-//     </div>
-//   );
-// }
-
-// export default TaskForm;
