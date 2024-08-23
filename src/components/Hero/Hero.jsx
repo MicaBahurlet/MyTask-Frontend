8120488ea9aa2ec0cd55bab82947e3e0fbfbe87e
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-08-23 15:24:18 -0300

    instal magin ui start wordRotate component

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,10 @@
 
-
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import ImgHero from "../../img/Hero.png";
 import { HeroContainer, HeroContent, HeroTitle, HeroDescription, HeroCTA, HeroImageContainer, HeroImage } from './HeroStyles.js';
-import { Fade, Flip, Hinge, JackInTheBox, Roll, Slide, Zoom } from 'react-awesome-reveal';
+// import {  Zoom } from 'react-awesome-reveal';
+// import WordRotateDemo from '../WordRotate/WordRotate.jsx';
 
 function Hero() {
     const token = localStorage.getItem('token'); // verifica token en localStorage
@@ -15,14 +14,15 @@ function Hero() {
             <HeroContent>
                 <HeroTitle>Administrá tu tiempo de manera eficiente.</HeroTitle>
                 <HeroDescription>MyTask. Crea y editá tus tareas</HeroDescription>
+                {/* <WordRotateDemo /> */}
                 <HeroCTA to={token ? '/dashboard' : '/login'}>
                     {token ? 'Crear Tarea' : 'Iniciar Sesión'}
                 </HeroCTA>
             </HeroContent>
             <HeroImageContainer>
-                <Zoom direction='in' duration={2000}>
+            
                     <HeroImage src={ImgHero} alt="Hero" />
-                </Zoom>
+                
              </HeroImageContainer>
         </HeroContainer>
     );
