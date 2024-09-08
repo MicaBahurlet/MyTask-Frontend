95b32923869db0a7791d4291e3fc60ff0256d667
Micaela Bahurlet <micabahurletgo@gmail.com>
2024-09-08 14:44:00 -0300

    add enviroments variables

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,11 +1,3 @@
-//todo: import variable de entorno de .env
-
-// export const BASE_URL = "https://mytask-production.up.railway.app"
-
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
-
-
-
-
 
 // http://localhost:8080
